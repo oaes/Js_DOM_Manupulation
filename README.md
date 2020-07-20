d8897426706acdb25acba292df863aa680ab65ae
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-07-20 22:51:57 +0600

    first project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Js_DOM_Manupulation
---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/index.scss'
+
+let click = document.getElementById('open')
+
+click.addEventListener('click', () => {
+    alert('working')
+})
