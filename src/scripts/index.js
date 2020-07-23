01b4c11072918c58cd47159625023921840f4102
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-07-24 00:12:50 +0600

    .

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,22 @@
-import '../styles/index.scss'
+import "../styles/index.scss";
 
-let click = document.getElementById('open')
 
-click.addEventListener('click', () => {
-    alert('working')
-})
+function $(selector){
+    return document.querySelector(selector)
+}
+
+let btn = $("#open");
+let colors = ["cyan", "tomato", "lime", "blue"];
+
+let i = 0;
+
+btn.addEventListener("click", function () {
+  let h1 = $(".heading");
+  h1.style.background = colors[i];
+
+  if (i > colors.length) {
+    i = 0;
+  } else {
+    i++;
+  }
+});
